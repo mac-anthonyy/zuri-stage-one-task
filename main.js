22c7e2163719487f7d854705bd49192ac4964837
mac-anthony <ekenemacanthony@gmail.com>
2023-09-10 10:29:28 +0100

    modified the index.html file and main.js file

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 
-const todayEl = document.getElementById('today');
+let todayEl = document.getElementById('today');
 const utcTimeEl = document.getElementById('utc');
 
 const date = new Date();
 
-const daysOfTheWeek = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+const daysOfTheWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
-const today = daysOfTheWeek[date.getDay()-1];
+const today = daysOfTheWeek[date.getDay()];
+console.log(date)
 
 
 const updateTime = () => {
   const date = new Date()
   utcTimeEl.textContent =  `UTC Time: ${date.getTime()}`
 } 
-
 
 todayEl.textContent =  `Current Day of the week: ${today}`;
 
